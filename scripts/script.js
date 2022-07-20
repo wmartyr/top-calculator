9263180309bbe05237b3a57d315c5de8ff9f15b8
Woodrow Martyr <woodrow.martyr@gmail.com>
2022-07-20 19:48:43 +0930

    Coded precision in JS

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,36 @@ let displayString = "";
 let hasDecimal = false;
 let operand1 = 0;
 let operand2 = 0;
+let operator = "none";
+let result = 0;
+let displayToBeCleared = false
 
 function showText(displayString) {
   display.textContent = displayString;
+}
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case "add":
+      return parseFloat((a + b).toPrecision(8));
+      break;
+    case "subtract":
+      return parseFloat((a - b).toPrecision(8));
+      break;
+    case "multiply":
+      return parseFloat((a * b).toPrecision(8));
+      break;
+    case "divide":
+      return parseFloat((a / b).toPrecision(8));
+      break;
+  }
+}
+
+function clearDisplay(displayClear) {
+  if (displayClear) {
+    displayString = "";
+  }
+  return false;
 }
 
 buttons.forEach((button) => {
@@ -23,54 +50,90 @@ buttons.forEach((button) => {
         showText(displayString);
         break;
       case "inverse":
+        if (displayString.startsWith("-")) {
+          displayString = displayString.slice(1);
+        } else {
+          displayString = "-" + displayString;
+        }
+        showText(displayString);
         break;
       case "divide":
+        if (operator === "none") {
+          operand1 = parseFloat(displayString);
+          operator = "divide";
+        } else {
+          operand2 = parseFloat(displayString);
+          result = operate(operator, operand1, operand2);
+          showText(result.toPrecision(8).toString());
+          operand1 = result;
+          operand2 = 0;
+        }
+        displayToBeCleared = true;
         break;
       case "key-7":
+        displayToBeCleared = clearDisplay(displayToBeCleared);
         displayString += "7";
         showText(displayString);
         break;
       case "key-8":
+        displayToBeCleared = clearDisplay(displayToBeCleared);
         displayString += "8";
         showText(displayString);
         break;
       case "key-9":
+        displayToBeCleared = clearDisplay(displayToBeCleared);
         displayString += "9";
         showText(displayString);
         break;
       case "multiply":
+        if (operator === "none") {
+          operand1 = parseFloat(displayString);
+          operator = "multiply";
+        } else {
+          operand2 = parseFloat(displayString);
+        }
+        displayToBeCleared = true;
         break;
       case "key-4":
+        displayToBeCleared = clearDisplay(displayToBeCleared);
         displayString += "4";
         showText(displayString);
         break;
       case "key-5":
+        displayToBeCleared = clearDisplay(displayToBeCleared);
         displayString += "5";
         showText(displayString);
         break;
       case "key-6":
+        displayToBeCleared = clearDisplay(displayToBeCleared);
         displayString += "6";
         showText(displayString);
         break;
       case "subtract":
         break;
       case "key-1":
+        displayToBeCleared = clearDisplay(displayToBeCleared);
         displayString += "1";
         showText(displayString);
         break;
       case "key-2":
+        displayToBeCleared = clearDisplay(displayToBeCleared);
         displayString += "2";
         showText(displayString);
         break;
       case "key-3":
+        displayToBeCleared = clearDisplay(displayToBeCleared);
         displayString += "3";
         showText(displayString);
         break;
       case "add":
         break;
       case "key-0":
-        displayString += "0";
-        showText(displayString);
+        displayToBeCleared = clearDisplay(displayToBeCleared);
+        if (!(displayString === "-" || displayString === "")) {
+          displayString += "0";
+          showText(displayString);
+        }
         break;
       case "dot":
         if (!hasDecimal) {
@@ -80,6 +143,12 @@ buttons.forEach((button) => {
         }
         break;
       case "equals":
+        operand2 = parseFloat(displayString);
+        result = operate(operator, operand1, operand2);
+        showText(result.toString());
+        operator = "none";
+        operand1 = 0;
+        operand2 = 0;
         break;
     }
   })
